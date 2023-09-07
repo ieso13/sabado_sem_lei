ba7fda4760d1dcd8b4ed42ea81433dcf5ffb30de
Adriel Zanatta <zanattaadriel@gmail.com>
2023-09-07 19:35:44 -0300

    callback add

--- SSL.py
+++ SSL.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, callback
 import dash_bootstrap_components as dbc
 
 
@@ -61,6 +61,7 @@ jogadores_df["Derrotas"] = (
 
 jogadores_df = jogadores_df.replace(np.nan, 0)
 
+
 # Classificação ao longo do tempo
 lista_jogos_completa_df = lista_jogos_df[["Rodada", "Jogador", "Pontos", "Gols"]]
 
@@ -71,12 +72,6 @@ lista_jogos_completa_df = (
     .stack()
     .reset_index()
 )
-
-linha_de_corte = jogadores_df.loc[jogadores_df["Presença"] >= PRESENÇA_MINIMA].index
-
-lista_jogos_completa_df = lista_jogos_completa_df[
-    lista_jogos_completa_df["Jogador"].isin(linha_de_corte)
-].reset_index()
 
 lista_jogos_completa_df["Pontos Acc"] = lista_jogos_completa_df.groupby("Jogador")[
     "Pontos"
@@ -93,10 +88,8 @@ lista_jogos_completa_df["Posição"] = (
     .add(1)
 )
 
-
 # Tabela de classificação final
 tabela = jogadores_df.copy().reset_index()
-tabela = tabela[tabela["Jogador"].isin(linha_de_corte)]
 tabela = tabela.sort_values(by=["Pontos"], ascending=False)
 tabela = tabela[
     [
@@ -130,31 +123,6 @@ tabela = tabela.rename(
 )
 
 # gráficos
-line_classificação = px.line(
-    lista_jogos_completa_df,
-    x="Rodada",
-    y="Posição",
-    color="Jogador",
-    template="none",
-    # title="Classificação",
-)
-line_classificação.update_traces(x0=1)
-line_classificação.update_yaxes(autorange="reversed")
-line_classificação.update_layout(showlegend=False, template="none", yaxis={"title": ""})
-for i, d in enumerate(line_classificação.data):
-    line_classificação.add_scatter(
-        x=[d.x[-1]],
-        y=[d.y[-1]],
-        mode="markers+text",
-        text=str(d.y[-1]) + "-" + d.name,
-        textfont=dict(color=d.line.color),
-        textposition="middle right",
-        marker=dict(color=d.line.color, size=12),
-        legendgroup=d.name,
-        showlegend=False,
-    )
-
-
 bar_pontos = px.bar(
     lista_jogos_completa_df,
     x="Pontos Acc",
@@ -207,8 +175,8 @@ app.layout = dbc.Container(
             [
                 dbc.Col(
                     html.H1(
-                        children="Sábado Sem Lei - 2022",
-                        style={"text-align": "center", "display": "flex"},
+                        "Sábado Sem Lei - 2022",
+                        style={"textAlign": "center"},
                     ),
                     width=3,
                 ),
@@ -216,40 +184,17 @@ app.layout = dbc.Container(
                     html.Div(
                         [
                             html.P("Presença Mínima"),
-                            dbc.Input(type="number", min=0, max=num_jogos, step=1),
+                            dbc.Input(
+                                id="filtro_presença",
+                                type="number",
+                                min=1,
+                                max=num_jogos,
+                                step=1,
+                                value=num_jogos // 2,
+                            ),
                         ]
                     ),
                     width=1,
-                ),
-                dbc.Col(
-                    html.Div(
-                        [
-                            dcc.RangeSlider(
-                                2011,
-                                2023,
-                                1,
-                                marks={
-                                    2011: "2021",
-                                    2012: "2012",
-                                    2013: "2013",
-                                    2014: "2014",
-                                    2015: "2015",
-                                    2016: "2016",
-                                    2017: "2017",
-                                    2018: "2018",
-                                    2019: "2019",
-                                    2020: "2020",
-                                    2021: "2021",
-                                    2022: "2022",
-                                    2023: "2023",
-                                },
-                                value=[2022, 2023],
-                                id="my-slider",
-                            ),
-                            html.Div(id="slider-output-container"),
-                        ]
-                    ),
-                    width=8,
                 ),
             ]
         ),
@@ -257,20 +202,14 @@ app.layout = dbc.Container(
             [
                 dbc.Col(
                     html.Div(
-                        dbc.Table.from_dataframe(
-                            tabela,
-                            size="sm",
-                            hover=True,
-                            style={"width": "100%", "height": "90vh"},
-                        ),
+                        id="table1",
+                        children=[],
                         style={"max-height": "90vh", "overflow": "auto"},
                     ),
                     width=4,
                 ),
                 dbc.Col(
-                    html.Div(
-                        dcc.Graph(figure=line_classificação, style={"height": "100vh"})
-                    ),
+                    html.Div(id="graph1", children=[]),
                     width=8,
                 ),
             ]
@@ -278,17 +217,73 @@ app.layout = dbc.Container(
         dbc.Row(
             [
                 dbc.Col(
-                    html.Div(dcc.Graph(figure=bar_pontos, style={"height": "95vh"})),
+                    html.Div(id="graph2", children=[]),
                     width=6,
                 ),
                 dbc.Col(
-                    html.Div(dcc.Graph(figure=bar_gols, style={"height": "95vh"})),
+                    html.Div(id="graph3", children=[]),
                     width=6,
                 ),
             ]
         ),
+        dcc.Store(id="store-data", data=[], storage_type="memory"),
     ],
     fluid=True,
 )
 
-app.run_server(debug=True, use_reloader=False)
+
+@callback(Output("store-data", "data"), Input("filtro_presença", "value"))
+def store_data(value):
+    linha_de_corte = jogadores_df.loc[jogadores_df["Presença"] >= value].index
+    lista_filtrada = lista_jogos_completa_df[
+        lista_jogos_completa_df["Jogador"].isin(linha_de_corte)
+    ].reset_index()
+    data = lista_filtrada.to_dict("records")
+    return data
+
+
+@callback(Output("graph1", "children"), Input("store-data", "data"))
+def create_graph1(data):
+    dff = pd.DataFrame(data)
+    line_classificação = px.line(
+        dff,
+        x="Rodada",
+        y="Posição",
+        range_x=[1, num_jogos + 10],
+        color="Jogador",
+        template="none",
+    )
+
+    line_classificação.update_yaxes(autorange="reversed")
+    line_classificação.update_layout(
+        showlegend=False, template="none", yaxis={"title": ""}
+    )
+    for i, d in enumerate(line_classificação.data):
+        line_classificação.add_scatter(
+            x=[d.x[-1]],
+            y=[d.y[-1]],
+            mode="markers+text",
+            text=str(d.y[-1]) + "-" + d.name,
+            textfont=dict(color=d.line.color),
+            textposition="middle right",
+            marker=dict(color=d.line.color, size=12),
+            legendgroup=d.name,
+            showlegend=False,
+        )
+
+    return dcc.Graph(figure=line_classificação, style={"height": "100vh"})
+
+
+@callback(Output("table1", "children"), Input("filtro_presença", "value"))
+def create_table1(value):
+    dff = tabela.loc[tabela["PJ"] >= value]
+    return dbc.Table.from_dataframe(
+        dff,
+        size="sm",
+        hover=True,
+        style={"width": "100%", "height": "90vh"},
+    )
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
